b13ee53afa62e2bcd2e05e8b260dc54af1f7122c
mehul <mmaheshwari2@wisc.edu>
2023-11-24 17:58:44 -0600

    updated m3 algo

--- m3/milestone3.py
+++ m3/milestone3.py
@@ -52,30 +52,51 @@ plt.savefig('gyroscope_plot.png')
 plt.show()
 
 
-
-
-
-
-# Smooth the data if necessary (e.g., using rolling mean or other smoothing techniques)
-# Example: Smoothing accelerometer data for 'accel_x', 'accel_y', 'accel_z'
+# Smooth the data if necessary (using rolling mean or other techniques)
+# Smoothing accelerometer data
 data['smooth_accel_x'] = data['accel_x'].rolling(window=5).mean()
 data['smooth_accel_y'] = data['accel_y'].rolling(window=5).mean()
 data['smooth_accel_z'] = data['accel_z'].rolling(window=5).mean()
 
+# Smoothing gyroscope data
+data['smooth_gyro_x'] = data['gyro_x'].rolling(window=5).mean()
+data['smooth_gyro_y'] = data['gyro_y'].rolling(window=5).mean()
+data['smooth_gyro_z'] = data['gyro_z'].rolling(window=5).mean()
+
+# Smoothing magnetometer data
+data['smooth_mag_x'] = data['mag_x'].rolling(window=5).mean()
+data['smooth_mag_y'] = data['mag_y'].rolling(window=5).mean()
+data['smooth_mag_z'] = data['mag_z'].rolling(window=5).mean()
+
 # Detect 90-degree turns
-# Example: Checking for turns based on changes in accelerometer data
-# You might need to adjust the threshold and criteria based on your data
-threshold = .75  # Adjust this threshold as needed
+# Calculate thresholds based on quantiles of smoothed data
+quantile_accel = .7 # Adjust these quantile values as needed
+quantile_gyro = .8
+quantile_mag = .73
 
-for index in range(1, len(data) - 1):
-    # Calculate differences between consecutive values
-    delta_x = data.at[index + 1, 'smooth_accel_x'] - data.at[index, 'smooth_accel_x']
-    delta_y = data.at[index + 1, 'smooth_accel_y'] - data.at[index, 'smooth_accel_y']
-    delta_z = data.at[index + 1, 'smooth_accel_z'] - data.at[index, 'smooth_accel_z']
+threshold_accel = data['smooth_accel_x'].quantile(quantile_accel)
+threshold_gyro = data['smooth_gyro_x'].quantile(quantile_gyro)
+threshold_mag = data['smooth_mag_x'].quantile(quantile_mag)
 
-    # Calculate the magnitude of the change
-    magnitude = np.sqrt(delta_x ** 2 + delta_y ** 2 + delta_z ** 2)
+for index in range(len(data) - 2):
+    # Calculate changes in accelerometer data
+    delta_accel_x = data.at[index + 1, 'smooth_accel_x'] - data.at[index, 'smooth_accel_x']
+    delta_accel_y = data.at[index + 1, 'smooth_accel_y'] - data.at[index, 'smooth_accel_y']
+    delta_accel_z = data.at[index + 1, 'smooth_accel_z'] - data.at[index, 'smooth_accel_z']
+    magnitude_accel = np.sqrt(delta_accel_x ** 2 + delta_accel_y ** 2 + delta_accel_z ** 2)
 
-    # Check if magnitude exceeds the threshold
-    if magnitude > threshold:
-        print(f"Potential 90-degree turn detected at index {index}")
+    # Calculate changes in gyroscope data
+    delta_gyro_x = data.at[index + 1, 'smooth_gyro_x'] - data.at[index, 'smooth_gyro_x']
+    delta_gyro_y = data.at[index + 1, 'smooth_gyro_y'] - data.at[index, 'smooth_gyro_y']
+    delta_gyro_z = data.at[index + 1, 'smooth_gyro_z'] - data.at[index, 'smooth_gyro_z']
+    magnitude_gyro = np.sqrt(delta_gyro_x ** 2 + delta_gyro_y ** 2 + delta_gyro_z ** 2)
+
+    # Calculate changes in magnetometer data
+    delta_mag_x = data.at[index + 1, 'smooth_mag_x'] - data.at[index, 'smooth_mag_x']
+    delta_mag_y = data.at[index + 1, 'smooth_mag_y'] - data.at[index, 'smooth_mag_y']
+    delta_mag_z = data.at[index + 1, 'smooth_mag_z'] - data.at[index, 'smooth_mag_z']
+    magnitude_mag = np.sqrt(delta_mag_x ** 2 + delta_mag_y ** 2 + delta_mag_z ** 2)
+
+    # Check if any of the magnitudes exceed the thresholds
+    if magnitude_accel > threshold_accel and magnitude_gyro > threshold_gyro and magnitude_mag > threshold_mag:
+        print(f"Potential 90-degree turn detected at time {data.at[index, 'timestamp']}")
