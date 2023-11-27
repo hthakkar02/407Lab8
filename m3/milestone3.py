6f1e8d9f350bedda84b831bfba1d7a4f5ff09b11
mehul <mmaheshwari2@wisc.edu>
2023-11-27 02:16:19 -0600

    M3 done

--- m3/milestone3.py
+++ m3/milestone3.py
@@ -2,32 +2,50 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-#read in cols
+# read in cols
 columns_to_use = ['timestamp', 'accel_x', 'accel_y', 'accel_z', 'gyro_x', 'gyro_y', 'gyro_z', 'mag_x', 'mag_y', 'mag_z']
 data = pd.read_csv('../lab8-dataset/TURNING.csv', header=0, usecols=columns_to_use)
 
-# Plot the accelerometer data
+# Plot the accelerometer data (both smoothed and unsmoothed)
 plt.figure(figsize=(12, 6))
 
-# Assuming 'accel_x', 'accel_y', 'accel_z' are the columns for accelerometer data
-plt.plot(data['accel_x'], label='accel_x')
-plt.plot(data['accel_y'], label='accel_y')
-plt.plot(data['accel_z'], label='accel_z')
+# Unsmoothed data
+plt.plot(data['accel_x'], label='accel_x (Unsmoothed)')
+plt.plot(data['accel_y'], label='accel_y (Unsmoothed)')
+plt.plot(data['accel_z'], label='accel_z (Unsmoothed)')
+
+# Smoothed data
+data['smooth_accel_x'] = data['accel_x'].rolling(window=5).mean()
+data['smooth_accel_y'] = data['accel_y'].rolling(window=5).mean()
+data['smooth_accel_z'] = data['accel_z'].rolling(window=5).mean()
+
+plt.plot(data['smooth_accel_x'], label='accel_x (Smoothed)')
+plt.plot(data['smooth_accel_y'], label='accel_y (Smoothed)')
+plt.plot(data['smooth_accel_z'], label='accel_z (Smoothed)')
 
 plt.xlabel('Timestamp')
 plt.ylabel('Acceleration')
 plt.title('Accelerometer Data')
 plt.legend()
 plt.savefig('accelerometer_plot.png')
+plt.show()
 
-
-# Plot the magnetometer data
+# Plot the magnetometer data (both smoothed and unsmoothed)
 plt.figure(figsize=(12, 6))
 
-# Assuming 'mag_x', 'mag_y', 'mag_z' are the columns for magnetometer data
-plt.plot(data['mag_x'], label='mag_x')
-plt.plot(data['mag_y'], label='mag_y')
-plt.plot(data['mag_z'], label='mag_z')
+# Unsmoothed data
+plt.plot(data['mag_x'], label='mag_x (Unsmoothed)')
+plt.plot(data['mag_y'], label='mag_y (Unsmoothed)')
+plt.plot(data['mag_z'], label='mag_z (Unsmoothed)')
+
+# Smoothed data
+data['smooth_mag_x'] = data['mag_x'].rolling(window=5).mean()
+data['smooth_mag_y'] = data['mag_y'].rolling(window=5).mean()
+data['smooth_mag_z'] = data['mag_z'].rolling(window=5).mean()
+
+plt.plot(data['smooth_mag_x'], label='mag_x (Smoothed)')
+plt.plot(data['smooth_mag_y'], label='mag_y (Smoothed)')
+plt.plot(data['smooth_mag_z'], label='mag_z (Smoothed)')
 
 plt.xlabel('Timestamp')
 plt.ylabel('Magnetometer')
@@ -36,13 +54,22 @@ plt.legend()
 plt.savefig('magnetometer_plot.png')
 plt.show()
 
-# Plot the gyroscope data
+# Plot the gyroscope data (both smoothed and unsmoothed)
 plt.figure(figsize=(12, 6))
 
-# Assuming 'gyro_x', 'gyro_y', 'gyro_z' are the columns for gyroscope data
-plt.plot(data['gyro_x'], label='gyro_x')
-plt.plot(data['gyro_y'], label='gyro_y')
-plt.plot(data['gyro_z'], label='gyro_z')
+# Unsmoothed data
+plt.plot(data['gyro_x'], label='gyro_x (Unsmoothed)')
+plt.plot(data['gyro_y'], label='gyro_y (Unsmoothed)')
+plt.plot(data['gyro_z'], label='gyro_z (Unsmoothed)')
+
+# Smoothed data
+data['smooth_gyro_x'] = data['gyro_x'].rolling(window=5).mean()
+data['smooth_gyro_y'] = data['gyro_y'].rolling(window=5).mean()
+data['smooth_gyro_z'] = data['gyro_z'].rolling(window=5).mean()
+
+plt.plot(data['smooth_gyro_x'], label='gyro_x (Smoothed)')
+plt.plot(data['smooth_gyro_y'], label='gyro_y (Smoothed)')
+plt.plot(data['smooth_gyro_z'], label='gyro_z (Smoothed)')
 
 plt.xlabel('Timestamp')
 plt.ylabel('Gyroscope')
@@ -50,28 +77,10 @@ plt.title('Gyroscope Data')
 plt.legend()
 plt.savefig('gyroscope_plot.png')
 plt.show()
-
-
-# Smooth the data if necessary (using rolling mean or other techniques)
-# Smoothing accelerometer data
-data['smooth_accel_x'] = data['accel_x'].rolling(window=5).mean()
-data['smooth_accel_y'] = data['accel_y'].rolling(window=5).mean()
-data['smooth_accel_z'] = data['accel_z'].rolling(window=5).mean()
-
-# Smoothing gyroscope data
-data['smooth_gyro_x'] = data['gyro_x'].rolling(window=5).mean()
-data['smooth_gyro_y'] = data['gyro_y'].rolling(window=5).mean()
-data['smooth_gyro_z'] = data['gyro_z'].rolling(window=5).mean()
-
-# Smoothing magnetometer data
-data['smooth_mag_x'] = data['mag_x'].rolling(window=5).mean()
-data['smooth_mag_y'] = data['mag_y'].rolling(window=5).mean()
-data['smooth_mag_z'] = data['mag_z'].rolling(window=5).mean()
-
 # Detect 90-degree turns
 # Calculate thresholds based on quantiles of smoothed data
 quantile_accel = .7 # Adjust these quantile values as needed
-quantile_gyro = .8
+quantile_gyro = .7
 quantile_mag = .73
 
 threshold_accel = data['smooth_accel_x'].quantile(quantile_accel)
